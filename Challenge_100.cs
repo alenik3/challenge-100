8d1483ed3870dd493c95be60bac48f8a3b6fb3c2
alenik3 <79413865+alenik3@users.noreply.github.com>
2021-02-21 17:40:36 +0100

    Add files via upload

---
+++ Challenge_100.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Challenge_100
+{
+        //challenge-100
+
+        static int[] FinMaxMin(int[] arr)
+        {
+            return new int[] { arr.Min(), arr.Max() };
+        }
+
+        static string NameShuffle(string s)
+        {
+            List<string> result = s.Split(' ').ToList();
+            result.Reverse();
+            return string.Join(" ", result); ;
+        }
+
+        static bool SameCase(string s)
+        {
+            return (s.Equals(s.ToUpper()) || s.Equals(s.ToLower()));
+        }
+
+        static bool IsIsogram(string s)
+        {
+            s = s.ToLower();
+            return !(s.Distinct().Count() < s.Length);
+        }
+
+ }
+
---
+++ Challenge_200.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+
+public class Challenge_200
+{
+    static string Maskify(string s)
+    {
+        return ReplacingBackCharacters(s, 4, '#');
+    }
+
+    static string ReplacingBackCharacters(string s, int i, char charToReplace)
+    {
+        if (string.IsNullOrEmpty(s)) return "";
+        // empty. return, throw whatever.
+
+        string returnString = "";
+        int length = s.Length;
+        string endString, tmpString;
+
+        if (length < i)
+            return s;
+        try
+        {
+            endString = s.Substring(length - i, i);
+            tmpString = s.Substring(0, length - i + 1);
+
+            StringBuilder sb = new StringBuilder(tmpString);
+
+            foreach (var c in tmpString.ToCharArray()) { sb.Replace(c, charToReplace); }
+
+            returnString = sb.ToString() + endString;
+
+        }
+        catch (ArgumentException e)
+        {
+            //throw whatever
+        }
+
+        return returnString;
+    }
+
+    static int Potatoes(string s)
+    {
+        int potatoesCount = 0;
+        string strPotatoe = "potato";
+
+        char[] charArr = s.ToCharArray();
+        char[] charArrPotatoe = strPotatoe.ToCharArray();
+        int potLenght = strPotatoe.Length;
+
+        int countOfMatches = 0;
+
+        foreach (char ch in charArr)
+        {
+            if (ch == charArrPotatoe[countOfMatches])
+            {
+                countOfMatches++;
+            }
+            if (potLenght == countOfMatches)
+            {
+                potatoesCount++;
+                countOfMatches = 0;
+            }
+        }
+
+        return potatoesCount;
+    }
+}
